1b5bd8911ac0296743d083342735a3a4ea471dce
arifurrahman01 <arifurrahman.now@gmail.com>
2022-11-07 23:52:18 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import { routes } from './Routes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Hello</h2>
+    <div className='px-3 md:px-8 lg:px-20' >
+      <RouterProvider router={routes}></RouterProvider>
+      <Toaster></Toaster>
     </div>
   );
 }
