e38c850de994a97d64a2dee7ae6bdbe1ec924ebc
arifurrahman01 <arifurrahman.now@gmail.com>
2022-11-10 23:49:50 +0600

    comment card modified

--- src/Pages/Reviews/ReviewRow.js
+++ src/Pages/Reviews/ReviewRow.js
@@ -6,8 +6,8 @@ const ReviewRow = ({ r }) => {
 
     return (
         <div className="card card-side bg-base-100 p-6 shadow my-3">
-            <figure><img src={user?.photoURL} className="rounded-full w-24" alt="user" /></figure>
-            <div className="card-body">
+            <figure className='w-1/4'><img src={user?.photoURL} className="rounded-full w-16" alt="user" /></figure>
+            <div className="card-body w-3/4">
                 <h2 className="card-title">{user?.displayName}</h2>
                 <p>{review}</p>
             </div>
